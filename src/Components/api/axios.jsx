83b181e2d652162c24a8aa4954416d9e97b8a3d2
KHAYRE-DDINE <ahrarkhirdin@gmail.com>
2026-08-10 09:50:34 +0100

    Update API URL to use Render in production

--- src/Components/api/axios.jsx
+++ src/Components/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.NODE_ENV === "production" ? "https://dashboard-aj8w.onrender.com" : "http://localhost:3001",
   withCredentials: false,
 });
